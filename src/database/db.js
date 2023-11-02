719c9e39386cd7da60e75b7fb40e1cd49d6e1125
F.Gonçalves <boriscasoy123@gmail.com>
2023-11-01 22:08:42 -0300

    include .env

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
+require("dotenv").config()
 
-const url = "mongodb+srv://fabiano123:fabiano123@cluster0.p2xagpb.mongodb.net/?retryWrites=true&w=majority"
+const url = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.p2xagpb.mongodb.net/?retryWrites=true&w=majority`
 
 async function connectDataBase() {
 	try {
